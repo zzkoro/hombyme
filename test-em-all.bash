0c4b2a6aff50e71527e832470a2c7066a0e317b8
junemp <junemp@gmail.com>
2021-03-21 23:08:36 +0900

    * chapter7 리액티브 마이크로서비스 개발

--- test-em-all.bash
+++ test-em-all.bash
@@ -10,6 +10,10 @@
 #
 : ${HOST=localhost}
 : ${PORT=8080}
+: ${PROD_ID_REVS_RECS=2}
+: ${PROD_ID_NOT_FOUND=14}
+: ${PROD_ID_NO_RECS=114}
+: ${PROD_ID_NO_REVS=214}
 
 function assertCurl() {
 
@@ -79,40 +83,133 @@ function waitForService() {
   done
 }
 
+function testCompositeCreated() {
+
+    # Expect that the Product Composite for productId $PROD_ID_REVS_RECS has been created with three recommendations and three reviews
+    if ! assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS_RECS -s"
+    then
+        echo -n "FAIL"
+        return 1
+    fi
+
+    set +e
+    assertEqual "$PROD_ID_REVS_RECS" $(echo $RESPONSE | jq .productId)
+    if [ "$?" -eq "1" ] ; then return 1; fi
+
+    assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
+    if [ "$?" -eq "1" ] ; then return 1; fi
+
+    assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
+    if [ "$?" -eq "1" ] ; then return 1; fi
+
+    set -e
+}
+
+function waitForMessageProcessing() {
+    echo "Wait for messages to be processed... "
+
+    # Give background processing some time to complete...
+    sleep 1
+
+    n=0
+    until testCompositeCreated
+    do
+        n=$((n + 1))
+        if [[ $n == 40 ]]
+        then
+            echo " Give up"
+            exit 1
+        else
+            sleep 6
+            echo -n ", retry #$n "
+        fi
+    done
+    echo "All messages are now processed!"
+}
+
+function recreateComposite() {
+    local productId=$1
+    local composite=$2
+
+    assertCurl 200 "curl -X DELETE http://$HOST:$PORT/product-composite/${productId} -s"
+    curl -X POST http://$HOST:$PORT/product-composite -H "Content-Type: application/json" --data "$composite"
+}
+
+function setupTestdata() {
+
+    body="{\"productId\":$PROD_ID_NO_RECS"
+    body+=\
+',"name":"product name A","weight":100, "reviews":[
+    {"reviewId":1,"author":"author 1","subject":"subject 1","content":"content 1"},
+    {"reviewId":2,"author":"author 2","subject":"subject 2","content":"content 2"},
+    {"reviewId":3,"author":"author 3","subject":"subject 3","content":"content 3"}
+]}'
+    recreateComposite "$PROD_ID_NO_RECS" "$body"
+
+    body="{\"productId\":$PROD_ID_NO_REVS"
+    body+=\
+',"name":"product name B","weight":200, "recommendations":[
+    {"recommendationId":1,"author":"author 1","rate":1,"content":"content 1"},
+    {"recommendationId":2,"author":"author 2","rate":2,"content":"content 2"},
+    {"recommendationId":3,"author":"author 3","rate":3,"content":"content 3"}
+]}'
+    recreateComposite "$PROD_ID_NO_REVS" "$body"
+
+
+    body="{\"productId\":$PROD_ID_REVS_RECS"
+    body+=\
+',"name":"product name C","weight":300, "recommendations":[
+        {"recommendationId":1,"author":"author 1","rate":1,"content":"content 1"},
+        {"recommendationId":2,"author":"author 2","rate":2,"content":"content 2"},
+        {"recommendationId":3,"author":"author 3","rate":3,"content":"content 3"}
+    ], "reviews":[
+        {"reviewId":1,"author":"author 1","subject":"subject 1","content":"content 1"},
+        {"reviewId":2,"author":"author 2","subject":"subject 2","content":"content 2"},
+        {"reviewId":3,"author":"author 3","subject":"subject 3","content":"content 3"}
+    ]}'
+    recreateComposite 1 "$body"
+
+}
+
 set -e
 
+echo "Start Tests:" `date`
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
 
 if [[ $@ == *"start"* ]]
 then
-  echo "Restarting the test environment..."
-  echo "$ docker-compose down"
-  docker-compose down
-  echo "$ docker-compose up -d"
-  docker-compose up -d
+    echo "Restarting the test environment..."
+    echo "$ docker-compose down --remove-orphans"
+    docker-compose down --remove-orphans
+    echo "$ docker-compose up -d"
+    docker-compose up -d
 fi
 
-waitForService http://$HOST:$PORT/product-composite/1
+waitForService curl http://$HOST:$PORT/actuator/health
+
+setupTestdata
+
+waitForMessageProcessing
 
 # Verify that a normal request works, expect three recommendations and three reviews
-assertCurl 200 "curl http://$HOST:$PORT/product-composite/1 -s"
-assertEqual 1 $(echo $RESPONSE | jq .productId)
+assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS_RECS -s"
+assertEqual "$PROD_ID_REVS_RECS" $(echo $RESPONSE | jq .productId)
 assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
 assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
 
-# Verify that a 404 (Not Found) error is returned for a non existing productId (13)
-assertCurl 404 "curl http://$HOST:$PORT/product-composite/13 -s"
+# Verify that a 404 (Not Found) error is returned for a non existing productId ($PROD_ID_NOT_FOUND)
+assertCurl 404 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NOT_FOUND -s"
 
-# Verify that no recommendations are returned for productId 113
-assertCurl 200 "curl http://$HOST:$PORT/product-composite/113 -s"
-assertEqual 113 $(echo $RESPONSE | jq .productId)
+# Verify that no recommendations are returned for productId $PROD_ID_NO_RECS
+assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NO_RECS -s"
+assertEqual "$PROD_ID_NO_RECS" $(echo $RESPONSE | jq .productId)
 assertEqual 0 $(echo $RESPONSE | jq ".recommendations | length")
 assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
 
-# Verify that no reviews are returned for productId 213
-assertCurl 200 "curl http://$HOST:$PORT/product-composite/213 -s"
-assertEqual 213 $(echo $RESPONSE | jq .productId)
+# Verify that no reviews are returned for productId $PROD_ID_NO_REVS
+assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NO_REVS -s"
+assertEqual $PROD_ID_NO_REVS $(echo $RESPONSE | jq .productId)
 assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
 assertEqual 0 $(echo $RESPONSE | jq ".reviews | length")
 
@@ -124,13 +221,12 @@ assertEqual "\"Invalid productId: -1\"" "$(echo $RESPONSE | jq .message)"
 assertCurl 400 "curl http://$HOST:$PORT/product-composite/invalidProductId -s"
 assertEqual "\"Type mismatch.\"" "$(echo $RESPONSE | jq .message)"
 
+echo "End, all tests OK:" `date`
 
 if [[ $@ == *"stop"* ]]
 then
-  echo "We are done, stopping the test environment..."
-  echo "$ docker-compose down"
-  docker-compose down
+    echo "Stopping the test environment..."
+    echo "$ docker-compose down --remove-orphans"
+    docker-compose down --remove-orphans
 fi
-
-echo "End:" `date`
 
